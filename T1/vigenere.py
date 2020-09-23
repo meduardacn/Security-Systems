00bbae9c4c12393294f0f8592bb2b022bccd7a36
meduardcn <m.eduardacn@gmail.com>
2020-09-23 13:33:33 -0300

    coincidenceIndexFor function ok

--- T1/vigenere.py
+++ T1/vigenere.py
@@ -1,5 +1,17 @@
+import time
+start_time = time.time()
+
+icPT = 0.072723
+
+def newFrequencyDict():	
+	return dict([('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 0),
+				 ('f', 0), ('g', 0), ('h', 0), ('i', 0), ('j', 0),
+				 ('k', 0), ('l', 0), ('m', 0), ('n', 0), ('o', 0), 
+				 ('p', 0), ('q', 0), ('r', 0), ('s', 0), ('t', 0),
+				 ('u', 0), ('v', 0), ('w', 0), ('x', 0), ('y', 0), ('z', 0)])
+
 def keywordLength(chipherText, attempts):
-	dic_ic = dict() # int -> [float] | possible keyword lenght -> [indexes of coincidence] 
+	dic_ic = dict() 
 	for i in range(1,attempts+1):	
 		dic_ic[i] = []
 		for j in range(i):	
@@ -8,26 +20,24 @@ def keywordLength(chipherText, attempts):
 			while k < len(chiperText):	
 				newText += chiperText[k]
 				k += i
-			dic_ic[i].append(newText)
+			dic_ic[i].append(coincidenceIndexFor(newText))
 	return dic_ic		
 
-
-
-def freq(chipherText):
-	for elem in chipherText:
-		value = listFreq[elem]
-		listFreq[elem] = value + 1
-
-icPT = 0.072723
-listFreq = dict([('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 0),
-				 ('f', 0), ('g', 0), ('h', 0), ('i', 0), ('j', 0),
-				 ('k', 0), ('l', 0), ('m', 0), ('n', 0), ('o', 0), 
-				 ('p', 0), ('q', 0), ('r', 0), ('s', 0), ('t', 0),
-				 ('u', 0), ('v', 0), ('w', 0), ('x', 0), ('y', 0), ('z', 0)])
+def coincidenceIndexFor(text):
+    # first step -> char probability
+	dictFreq = newFrequencyDict()
+	for elem in text:
+		dictFreq[elem] += 1
+	ic = 0
+	for k, v in dictFreq.items():	
+		probability = ( v * (v-1)) / ( len(text) * (len(text)-1))
+		ic += probability
+	return round(ic,6)
 
 
 chiperText = input()
-
-print( keywordLength(chiperText, 4) )
-
+print("tamanho", len(chiperText))
+# print(coincidenceIndexFor(chiperText))
+print( keywordLength(chiperText, 20))
+print("--- %s seconds ---" % (time.time() - start_time))
 
