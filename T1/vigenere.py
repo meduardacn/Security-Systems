7dd2e71cac855e7ae518621391d484ca63edafe1
meduardcn <m.eduardacn@gmail.com>
2020-09-24 20:04:19 -0300

    not working

--- T1/vigenere.py
+++ T1/vigenere.py
@@ -2,7 +2,6 @@ import time
 
 start_time = time.time()
 icPT = 0.072723
-
 def newFrequencyDict():	
 	return dict([('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 0),
 				 ('f', 0), ('g', 0), ('h', 0), ('i', 0), ('j', 0),
@@ -58,10 +57,6 @@ def mostFrequentLetter(text):
 			biggerFreq = v
 	return letter
 
-def unshiftLetter(letter):	
-	# unshifted = 'a' - letter
-    return 'z'
-
 def findKey(chiperText):	
 	lenght = keywordLength(chiperText,20)	
 	texts = dict()
@@ -72,20 +67,43 @@ def findKey(chiperText):
 			newText += chiperText[k]
 			k += lenght
 		texts[i] = newText
-	key = ""
+	shifts = []
 	for k, v in texts.items():	
 		letter = mostFrequentLetter(v)
-		key += unshiftLetter(letter)
-	return key
+		shifts.append(alphabet[letter])
+	return shifts
+
+def unshiftLetter(letter, shifts):	
+	valueOfL = abs(alphabet[letter] + shifts)
+	return alphabet[valueOfL%25]
 
 def decrypt(chiperText):	
-	keyWord = findKey(chiperText)
+	shifts = findKey(chiperText)
 	clearText = ""
-	for elem in chiperText:
-		clearText += unshiftLetter(elem)
+	i = 0
+	j = 0
+	while i < len(chiperText):	
+		key = shifts[j]
+		letter = chiperText[i]
+		clearText += unshiftLetter(letter,key)
+
+		i += 1	
+		j += 1	
+		if j == len(shifts):	
+			j = 0
+    		
 	print(clearText)
 
-
+alphabet = dict([('a', 0), ('b', 1), ('c', 2), ('d', 3), ('e', 4),
+				 ('f', 5), ('g', 6), ('h', 7), ('i', 8), ('j', 9),
+				 ('k', 10), ('l', 11), ('m', 12), ('n', 13), ('o', 14), 
+				 ('p', 15), ('q', 16), ('r', 17), ('s', 18), ('t', 19),
+				 ('u', 20), ('v', 21), ('w', 22), ('x', 23), ('y', 24), ('z', 25),
+				 (0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'),
+				 (5, 'f'), (6, 'g'), (7, 'h'), (8, 'i'), (9, 'j'),
+				 (10, 'k'), (11, 'l'), (12, 'm'), (13, 'n'), (14, 'o'), 
+				 (15, 'p'), (16, 'q'), (17, 'r'), (18, 's'), (19, 't'),
+				 (20, 'u'), (21, 'v'), (22, 'w'), (23, 'x'), (24, 'y'), (25, 'z')])
 chiperText = input()
 print("tamanho", len(chiperText))
 decrypt(chiperText)
