f3aa82be46701df9749c421cc1eb6fde86f834ff
meduardcn <m.eduardacn@gmail.com>
2020-09-25 00:18:18 -0300

    updates

--- T1/vigenere.py
+++ T1/vigenere.py
@@ -10,41 +10,42 @@ def newFrequencyDict():
 				 ('u', 0), ('v', 0), ('w', 0), ('x', 0), ('y', 0), ('z', 0)])
 
 def coincidenceIndexFor(text):
-    # first step -> char probability
 	dictFreq = newFrequencyDict()
 	for elem in text:
 		dictFreq[elem] += 1
 	ic = 0
 	for k, v in dictFreq.items():	
-		probability = ( v * (v-1)) / ( len(text) * (len(text)-1))
+		probability =  v * (v-1)
 		ic += probability
-	return ic
+	return (ic / ( len(text) * (len(text)-1)))
 
 def keywordLength(chipherText, attempts):
 	dic_ic = dict() 
-	dic_closerKey = dict()
 	for i in range(1,attempts+1):	
 		dic_ic[i] = []
-		dic_closerKey[i] = []
 		for j in range(i):	
 			newText = ""
 			k = j
-			while k < len(chiperText):	
-				newText += chiperText[k]
+			while k < len(cipherText):	
+				newText += cipherText[k]
 				k += i
 
 			ic = coincidenceIndexFor(newText)
-			if abs(icPT - ic) < 0.01:	
-				print(i)
-				return i 
-	# 			tupla = (ic,abs(icPT-ic))
-	# 			dic_closerKey[i].append(tupla)
-	# 		dic_ic[i].append(ic)
-	# #-------------------------------------------------------------
-	# for k, v in dic_closerKey.items():	
-	# 	if v != []:	
-	# 		return k
-
+			dic_ic[i].append(ic)
+	#-------------------------------------------------------------
+	largestIC = 0 
+	largestLength = 0 
+	for k, v in dic_ic.items():	
+		average =  0
+		for elem in v:	
+			average += elem
+		average = average/len(v)
+		if average > largestIC:	
+			largestIC = average
+			largestLength = k
+	print("key length", largestLength)
+	return largestLength
+		
 def mostFrequentLetter(text):	
 	dictFreq = newFrequencyDict()
 	for elem in text:
@@ -55,56 +56,81 @@ def mostFrequentLetter(text):
 		if v > biggerFreq:	
 			letter = k
 			biggerFreq = v
-	return letter
+	
+	dictFreq[letter] = 0
+	biggerFreq = 0
+	sletter = ""
+	for k, v in dictFreq.items():	
+		if v > biggerFreq:	
+			sletter = k
+			biggerFreq = v
 
-def findKey(chiperText):	
-	lenght = keywordLength(chiperText,20)	
+	return (letter, sletter)
+
+def findKey(cipherText):	
+	lenght = keywordLength(cipherText,20)	
 	texts = dict()
 	for i in range(lenght):	
 		newText = ""
 		k = i
-		while k < len(chiperText):	
-			newText += chiperText[k]
+		while k < len(cipherText):	
+			newText += cipherText[k]
 			k += lenght
 		texts[i] = newText
-	shifts = []
+	mostFrequent = ""
+	secondMost = ""
 	for k, v in texts.items():	
-		letter = mostFrequentLetter(v)
-		shifts.append(alphabet[letter])
-	return shifts
+		f, s  = mostFrequentLetter(v)
+		mostFrequent += f
+		secondMost += s
+	return(mostFrequent, secondMost)
 
-def unshiftLetter(letter, shifts):	
-	valueOfL = abs(alphabet[letter] + shifts)
-	return alphabet[valueOfL%25]
+def unshiftLetter(letter, key):	
+	valueOfL = abs(alphabet[letter] - alphabet[key])
+	print(valueOfL,valueOfL%26)
+	# return alphabet[valueOfL%25]
 
-def decrypt(chiperText):	
-	shifts = findKey(chiperText)
+def possibleKeys(cipherText):	
+	first, second = findKey(cipherText)
+	print(first, second)
+	tuples = []
+	for i in range(len(first)):	
+		tuples.append([first[i],second[i]])	
+
+	for elem in tuples:	
+		print(elem)
+
+
+def decrypt(cipherText, keyword):	
 	clearText = ""
 	i = 0
 	j = 0
-	while i < len(chiperText):	
-		key = shifts[j]
-		letter = chiperText[i]
-		clearText += unshiftLetter(letter,key)
-
+	print(keyword)
+	while i < len(cipherText):	
+		key = keyword[j]
+		letter = cipherText[i]
+		
+		clearText += 'x'
 		i += 1	
 		j += 1	
-		if j == len(shifts):	
+		if j == len(keyword):	
 			j = 0
     		
 	print(clearText)
 
-alphabet = dict([('a', 0), ('b', 1), ('c', 2), ('d', 3), ('e', 4),
-				 ('f', 5), ('g', 6), ('h', 7), ('i', 8), ('j', 9),
-				 ('k', 10), ('l', 11), ('m', 12), ('n', 13), ('o', 14), 
-				 ('p', 15), ('q', 16), ('r', 17), ('s', 18), ('t', 19),
-				 ('u', 20), ('v', 21), ('w', 22), ('x', 23), ('y', 24), ('z', 25),
-				 (0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'),
-				 (5, 'f'), (6, 'g'), (7, 'h'), (8, 'i'), (9, 'j'),
-				 (10, 'k'), (11, 'l'), (12, 'm'), (13, 'n'), (14, 'o'), 
-				 (15, 'p'), (16, 'q'), (17, 'r'), (18, 's'), (19, 't'),
-				 (20, 'u'), (21, 'v'), (22, 'w'), (23, 'x'), (24, 'y'), (25, 'z')])
-chiperText = input()
-print("tamanho", len(chiperText))
-decrypt(chiperText)
+alphabet = dict([('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5),
+				 ('f', 6), ('g', 7), ('h', 8), ('i', 9), ('j', 10),
+				 ('k', 11), ('l', 12), ('m', 13), ('n', 14), ('o', 15), 
+				 ('p', 16), ('q', 17), ('r', 18), ('s', 19), ('t', 20),
+				 ('u', 21), ('v', 22), ('w', 23), ('x', 24), ('y', 25), ('z', 26),
+				 (1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'e'),
+				 (6, 'f'), (7, 'g'), (8, 'h'), (9, 'i'), (10, 'j'),
+				 (11, 'k'), (12, 'l'), (13, 'm'), (14, 'n'), (15, 'o'), 
+				 (16, 'p'), (17, 'q'), (18, 'r'), (19, 's'), (20, 't'),
+				 (21, 'u'), (22, 'v'), (23, 'w'), (24, 'x'), (25, 'y'), (26, 'z')])
+
+cipherText = input()
+print("tamanho", len(cipherText))
+possibleKeys(cipherText)
+decrypt(cipherText, "avelino")
 print("--- %s seconds ---" % (time.time() - start_time))
