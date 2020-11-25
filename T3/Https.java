d395e2ba03467d6edccf4d9684a9d8608ec84569
meduardcn <m.eduardacn@gmail.com>
2020-11-25 17:38:11 -0300

    Etapa2

--- T3/Https.java
+++ T3/Https.java
@@ -1,5 +1,5 @@
 // java Https message key
-// java Https E1DD68F3DF6E014DD50FCAE334CB63D1F0CA66608F48E38E9A40E31E9D266C985B708A5C106F3CC208083F547B19D9B2C5A9E4F5DAF2F5208FE3A1D3F01143D3A94DF90EA450C414FB1D127D9E1447A726B58E86CD7E81A089F59AC089A4771BD360EA54E14DC81E12A677DAB1A700CB AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+// java Https 8A1DE7DD177FAE564A696092F36D672CA705ABFB171B940E9118D357FE4C31E2CB657F889745AA4A038B08477EFBA6A005C9FB45E2967E8DAC7FE34FFDC6B16388FF6F4033E99607F335814937433086C188CCC43652408636988743EC6EE7BD12B8E1A3C7EF1861EDE28D9F9EAC668E FAE9223FEB4CA59080BFB1378FEA56F0
 import java.util.Scanner;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
@@ -8,6 +8,8 @@ import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 
 public class Https {
+    public static BigInteger p;
+    public static BigInteger g;
 
     public  static byte[] hexStringToByteArray(String s) {
         int len = s.length();
@@ -59,21 +61,34 @@ public class Https {
         }
         return deciphered;
     }
+    public static void A(){
+        int a = 1434365;
+        BigInteger A = g.pow(a);
+        A = A.remainder(p);
+        System.out.println("A"+ A);
+        System.out.println(byteArrayToHexString(A.toByteArray()));  
+    }
 
     public static void main(String[] args) {
+        String hexP = "0B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371";
+        String hexG = "0A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5";
+        p = new BigInteger(hexP, 16);
+        g = new BigInteger(hexG, 16);
+        // A();
+        // -----------------------------
         String message = args[0];
         byte[] key = hexStringToByteArray(args[1]);        
         byte[] iv = hexStringToByteArray( message.substring(0, 32) );
         byte[] msg = hexStringToByteArray( message.substring(32) );
 
-
         byte[] decryptedBytes = decrypt(msg, key, iv);
         String decryptedString = hexStringToString(byteArrayToHexString(decryptedBytes));
-        
-        System.out.println(key);    
-        System.out.println(iv + " " + msg);
-
         System.out.println("\n" + decryptedString); 
+
+        StringBuilder sb = new StringBuilder(decryptedString);  
+        decryptedString  = sb.reverse().toString();  
+        System.out.println("\n" + decryptedString ); 
+        
 
     }
 }
